2e7cdb79d0c8c2ee9fc8fedb2cac78017a33a990
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-29 12:25:58 -0500

    render submit page

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
 
 // on submit (history.push) (dispatch)
@@ -8,18 +8,20 @@ import { useHistory } from "react-router-dom";
 
 
 function Submit(){
-
+    //get redux state
     const feedback = useSelector(store => store.feedback)
+    //check for data
+    console.log('feedback', feedback);
 
     return(
         // Returning page format
         //Eventually try sweet alerts
         <article name="reviewPage">
             <h1>Review Your Feedback</h1>
-            <p>Feelings: 5</p>
-            <p>Understanding: 5</p>
-            <p>Support: 5</p>
-            <p>Comments: I love this stuff!</p>
+            <p>Feeling: {feedback.feeling}</p>
+            <p>Understanding: {feedback.understanding}</p>
+            <p>Support: {feedback.support}</p>
+            <p>Comments: {feedback.comments}.</p>
             {/* sends data  */}
             <button>Submit</button>
         </article>
